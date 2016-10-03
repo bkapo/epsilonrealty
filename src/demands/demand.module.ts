6a60b70f3e657fcccd8a24d81ee7ecdc40d59a72
bkapo <bkapo@outlook.com>
2016-10-03 23:25:07 +0300

    refa

--- src/demands/demand.module.ts
+++ src/demands/demand.module.ts
@@ -2,6 +2,7 @@ import { NgModule }           from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 
 import { SharedModule }        from '../shared/shared.module';
+import { RealEstatePropertyModule } from '../realestate-properties/realestate-property.module'
 
 import { DemandCardComponent } from './demand-card.component/demand-card.component';
 import { DemandAddComponent } from './demand-add.component/demand-add.component';
@@ -13,6 +14,7 @@ import { DemandRealEstateMatching } from './demand-realestate-matching.component
 @NgModule({
   imports:[ 
         SharedModule,
+        RealEstatePropertyModule,
         IonicModule.forRoot(DemandCardComponent), 
         IonicModule.forRoot(DemandAddComponent),
         IonicModule.forRoot(DemandListPage), 
