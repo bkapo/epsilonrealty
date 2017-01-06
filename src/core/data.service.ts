2e07014bfc27db75a6684dacdc15a0ea4bdef1d8
bkapo <bkapo@outlook.com>
2017-01-06 13:31:30 +0200

    app script 1

--- src/core/data.service.ts
+++ src/core/data.service.ts
@@ -1,6 +1,8 @@
 
-export const INVOLVEDPARTY_API_URL = 'https://realestatecorewebapi.azurewebsites.net/api/InvolvedParties';
+//export const INVOLVEDPARTY_API_URL = 'http://localhost:5000/api/InvolvedParties';
+//export const ESTATEPROPERTIES_API_URL = 'http://localhost:5000/api/realestateproperties';
 
-export const ESTATEPROPERTIES_API_URL = 'https://realestatecorewebapi.azurewebsites.net/api/realestateproperties';
+export const INVOLVEDPARTY_API_URL = 'https://realestatecorewebapi.azurewebsites.net/api/InvolvedParties';
+export const ESTATEPROPERTIES_API_URL = 'https://realestatecorewebapi.azurewebsites.net//realestateproperties';
 
 
