e9c7b27ae2240053b1e02ba2b5cfb784f11244fc
bkapo <bkapo@outlook.com>
2017-01-11 22:38:46 +0200

    spelling

--- src/core/data.service.ts
+++ src/core/data.service.ts
@@ -3,6 +3,6 @@
 //export const ESTATEPROPERTIES_API_URL = 'http://localhost:5000/api/realestateproperties';
 
 export const INVOLVEDPARTY_API_URL = 'https://realestatecorewebapi.azurewebsites.net/api/InvolvedParties';
-export const ESTATEPROPERTIES_API_URL = 'https://realestatecorewebapi.azurewebsites.net//realestateproperties';
+export const ESTATEPROPERTIES_API_URL = 'https://realestatecorewebapi.azurewebsites.net/api/realestateproperties';
 
 
