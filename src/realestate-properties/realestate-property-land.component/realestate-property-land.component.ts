392bba1593576342bc77e469eb9d3b9d32b98550
bkapo <bkapo@outlook.com>
2017-03-09 00:21:53 +0200

    add land component

---
+++ src/realestate-properties/realestate-property-land.component/realestate-property-land.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RealEstatePropertyComponent } from '../realestate-property.component/realestate-property.component'
+
+@Component({
+  selector: 'realestate-property-land',
+  templateUrl: 'realestate-property-land.component.html'
+})
+
+export class RealEstatePropertyLandComponent extends RealEstatePropertyComponent {
+
+}
